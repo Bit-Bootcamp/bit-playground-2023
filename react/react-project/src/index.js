64915ec549587ecba410f0549868bce8af400cee
hamasaadwn <hamasaadwn@gmail.com>
2023-03-15 22:55:21 +0300

    fixed errors

--- react/react-project/src/index.js
+++ react/react-project/src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import { Provider } from "react-redux";
-import { store } from "./store";
-
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
