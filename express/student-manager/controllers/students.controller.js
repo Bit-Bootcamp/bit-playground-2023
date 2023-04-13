14b8bff3e71dc87be06d85870d9810bd605b339f
hamasaadwn <hamasaadwn@gmail.com>
2023-04-13 10:43:20 +0300

    added filter

--- express/student-manager/controllers/students.controller.js
+++ express/student-manager/controllers/students.controller.js
@@ -2,8 +2,18 @@ import Student from "../models/students.models.js";
 
 export const getStudents = async (req, res) => {
   try {
-    const students = await Student.find();
-    res.json({ status: "success", data: students });
+    // const filteredStudent = await Student.find()
+    //   .where("isActive")
+    //   .equals(true)
+    //   .where("age")
+    //   .gte(12);
+
+    let query = JSON.stringify(req.query);
+    query = query.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+
+    const students = await Student.find(JSON.parse(query));
+
+    res.json({ status: "success", results: students.length, data: students });
   } catch (err) {
     res.status(400).json({ status: "error", data: err });
   }
