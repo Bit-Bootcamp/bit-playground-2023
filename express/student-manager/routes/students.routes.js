9cea29bb6753fa23d8f8ba36ae7433bd026126d7
hamasaadwn <hamasaadwn@gmail.com>
2023-04-25 11:18:41 +0300

    swagger schemas

--- express/student-manager/routes/students.routes.js
+++ express/student-manager/routes/students.routes.js
@@ -5,6 +5,36 @@ import {
   getStudents,
 } from "../controllers/students.controller.js";
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    students:
+ *      type: object
+ *      required:
+ *        - fullName
+ *        - age
+ *      properties:
+ *        _id:
+ *          type: ObjectId
+ *          description: mongodb default object id
+ *        fullName:
+ *          type: string
+ *        isActive:
+ *          type: boolean
+ *          description: a boolean describes if the student is still studying
+ *        age:
+ *          type: number
+ *        startDate:
+ *          type: string
+ *          format: date
+ *          description: the date the student started studying
+ *      example:
+ *        _id: 64367315629e7cd7ce99836f
+ *        fullName: danita chalke
+ *        isActive: false
+ *
+ */
 const router = Router();
 
 router.route("/").post(addStudent).get(getStudents);
